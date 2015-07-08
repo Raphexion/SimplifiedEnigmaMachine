879672b8dcb3412559915c10b27790ab3d71e876
Niklas Johansson <niklas.johansson@rte.se>
2015-07-08 12:04:30 +0200

    Make it possible to rotate the wheel
    A wheel maps from one character to another. However,
    we often want to do a simple addition/subtration
    (called "rotate") to the character before we do the
    mapping.

--- Wheel.hs
+++ Wheel.hs
@@ -1,8 +1,16 @@
 -- | An Enigma wheel maps a enigma character to another
+
 module Wheel where
 
 import EnigmaChars
 import qualified Data.Map as M
 
--- | The main purpose of a wheel is to map a character to another,
+-- | The main purpose of a wheel is to map a enigma character to another,
+-- | this is how we can obscure text
+
 type Wheel         = M.Map EnigmaChar EnigmaChar
+
+-- | Allow rotate of enigma characters
+
+safeRotate :: Int -> EnigmaChar -> EnigmaChar
+safeRotate ticks = modEnigma . (+ticks) . fromEnum
